786a741b71ef5b7db8c406f3f710c89b062b0f4e
Arka-Sengupta <arka.sengupta.06@gmail.com>
2026-04-06 10:06:18 +0530

    final-build

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,17 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
+
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        tutorial: resolve(__dirname, 'tutorial.html')
+      }
+    }
+  },
+
   base: './',
   assetsInclude: ['**/*.glsl'],
   server: {
